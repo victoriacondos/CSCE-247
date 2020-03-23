bb9e672fef821f03d8910599b8308d6189dbb6e1
Victoria Condos <vcondos@email.sc.edu>
2020-03-23 19:20:17 -0400

    Victoria created TicketStandardClass

--- src/TicketDecorator.java
+++ src/TicketDecorator.java
@@ -3,6 +3,6 @@
  * @author Victoria Condos
  * Purpose: Abstract base class that decorations inherit toString method from
  */
-public abstract class TicketDecorator {
+public abstract class TicketDecorator extends Ticket {
 	public abstract String toString();
 }
---
+++ src/TicketStandard.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ * @author Victoria Condos
+ * Purpose: A ticket decoration for an event that is not IMAX or 3D
+ */
+public class TicketStandard {
+
+}
