d2abd0a1fe90709fd967b133796d7af8f6f96c84
elizabethstevenson <efs1@email.sc.edu>
2020-03-24 10:47:51 -0400

    adding inventory and seating

--- src/Event.java
+++ src/Event.java
@@ -7,14 +7,27 @@ public class Event {
 	protected String location;
 	protected double time;
 	protected double price;
-	protected Seating seating;
+	protected Seating seating;//what are we doing with this variable?
 	
+	/**
+	 * Purpose: to create a new event
+	 * @param title: name of event
+	 * @param location: place of event
+	 * @param time: time of event
+	 */
 	public Event(String title, String location, double time) {
 		this.title = title;
 		this.location = location;
 		this.time = time;
 	}
 	
+	/**
+	 * Purpose: returns the title, location, time, and price of event
+	 * @return title: name of event
+	 * @return location: place of event
+	 * @return time: time of event
+	 * @return price: price of event
+	 */
 	public String toString() {
 		return "Title: " + this.title +
 				" Location: " + this.location + 
