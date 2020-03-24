c060c5ad52d602cc0a9372c1ffe8cf9d55cb7fd8
Victoria Condos <vcondos@email.sc.edu>
2020-03-24 15:56:50 -0400

    Victoria added code to discount classes

--- src/MilitaryDiscount.java
+++ src/MilitaryDiscount.java
@@ -1,4 +1,32 @@
-
+/**
+ * 
+ * @author Victoria Condos
+ * Purpose: applies discount to cost for a military account purchasing a ticket
+ */
 public class MilitaryDiscount {
-
+	private Ticket ticket;
+	private final double PERCENT_OF_TOTAL = .75;
+	
+	/**
+	 * 
+	 * @param ticket
+	 */
+	public MilitaryDiscount(Ticket ticket) {
+		this.ticket = ticket;
+	}
+	
+	/**
+	 * 
+	 */
+	public String toString() {
+		return ticket.toString() + " Discout: Military";
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public double getCost() {
+		return ticket.getCost() * PERCENT_OF_TOTAL;
+	}
 }
--- src/SeniorDiscount.java
+++ src/SeniorDiscount.java
@@ -1,20 +1,32 @@
 /**
  * 
  * @author Victoria Condos
- * Purpose: gives a discount for a senior citizen account purchasing a ticket
+ * Purpose: applies discount to cost for a senior citizen account purchasing a ticket
  */
 public class SeniorDiscount {
 	private Ticket ticket;
-	private final double PERCENT_OF_TOTAL = .85; 
+	private final double PERCENT_OF_TOTAL = .80; 
 	
+	/**
+	 * Purpose:
+	 * @param ticket
+	 */
 	public SeniorDiscount(Ticket ticket) {
 		this.ticket = ticket;
 	} 
 	
+	/**
+	 * Purpose:
+	 * @return
+	 */
 	public String toString() {
-		return ticket.toString() + "Discount: Senior Citizen";
+		return ticket.toString() + " Discount: Senior Citizen";
 	}
 	
+	/**
+	 * Purpose:
+	 * @return
+	 */
 	public double getCost() {
 		return ticket.getCost() * PERCENT_OF_TOTAL;
 	}
