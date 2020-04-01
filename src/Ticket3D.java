999f90c785eaf69c955e921bcdda5fd06de4cb5f
Victoria Condos <vcondos@email.sc.edu>
2020-04-01 12:08:45 -0400

    finished code for Standard, 3D, and IMAX ticket decorations

--- src/Ticket3D.java
+++ src/Ticket3D.java
@@ -1,4 +1,30 @@
-
+/**
+ * 
+ * @author Victoria Condos
+ * Purpose: A ticket decoration for an event with 3D viewing type
+ */
 public class Ticket3D {
-
+	private Ticket ticket;
+	private final double PRICE_3D = 15.0;
+	/**
+	 * Purpose: constructor that sets private ticket to param ticket
+	 * @param ticket: the ticket that is being decorated
+	 */
+	public Ticket3D(Ticket ticket) {
+		this.ticket = ticket;
+	}
+	/**
+	 * Purpose: appends additional info to end of ticket's toString method
+	 * return: string of all of tickets info
+	 */
+	public String toString() {
+		return ticket.toString() + " Viewing Type: 3D";
+	}
+	/**
+	 * Purpose: adds additional cost to existing ticket price
+	 * @return double of ticket's new price
+	 */
+	public double getCost() {
+		return ticket.getCost() + PRICE_3D;
+	}
 }
--- src/TicketIMAX.java
+++ src/TicketIMAX.java
@@ -1,4 +1,30 @@
-
+/**
+ * 
+ * @author Victoria Condos
+ * Purpose: A ticket decoration for an event with IMAX viewing type
+ */
 public class TicketIMAX {
-
+	private Ticket ticket;
+	private final double IMAX_PRICE = 12.0;
+	/**
+	 * Purpose: constructor that sets private ticket to param ticket
+	 * @param ticket: the ticket that is being decorated
+	 */
+	public TicketIMAX(Ticket ticket) {
+		this.ticket = ticket;
+	}
+	/**
+	 * Purpose: appends additional info to end of ticket's toString method
+	 * return: string of all of tickets info
+	 */
+	public String toString() {
+		return ticket.toString() + " Viewing Type: IMAX";
+	}
+	/**
+	 * Purpose: adds additional cost to existing ticket price
+	 * @return double of ticket's new price
+	 */
+	public double getCost() {
+		return ticket.getCost() + IMAX_PRICE;
+	}
 }
