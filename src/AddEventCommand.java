f50e24b551179b2ef2853517ae1800c486fa64b4
Victoria Condos <43612035+victoriacondos@users.noreply.github.com>
2020-04-06 14:45:36 -0400

    Added javadoc to AddEventCommand

--- src/AddEventCommand.java
+++ src/AddEventCommand.java
@@ -1,5 +1,11 @@
-
+/**
+* Author: Brett Logeais
+* Purpose: command that allows for events to be added to system
+*/
 public class AddEventCommand implements Command{
+	/**
+	* Purpose: prompts for event to be added based on its type (Movie, Concert, Theatre Event)
+	*/
 	public void execute() {
 		System.out.println("\t Add an event\n\n");
 		Event event = Dialogue.createEvent();
