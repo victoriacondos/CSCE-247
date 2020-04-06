6225789e92bc7e369033cd9975736daa2a118ab3
Victoria Condos <43612035+victoriacondos@users.noreply.github.com>
2020-04-06 15:08:30 -0400

    Added some Javadoc to DatabaseObject

--- src/DatabaseObject.java
+++ src/DatabaseObject.java
@@ -1,12 +1,26 @@
+/**
+* author: Brett Logeais
+* Purpose: 
+*/
 import org.json.simple.JSONObject;
 
 public class DatabaseObject {
-	
+	/**
+	* Purpose: default constructor for DatabaseObject TODO-??
+	*/
 	public DatabaseObject() {}
 	
+	/**
+	* Purpose: constructor for object from JSON file
+	* @param objectJSON: an object from a JSON file
+	*/
 	public DatabaseObject(JSONObject objectJSON) {
 	}
 	
+	/**
+	* Purpose:
+	* return: null
+	*/
 	public JSONObject toJSON() {
 		return null;
 	}
