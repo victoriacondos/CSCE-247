c71275fdf4235ed20bcd9348cdaaf71721dfcedc
elizabethstevenson <60235792+elizabethstevenson@users.noreply.github.com>
2020-04-06 16:27:14 -0400

    added javadoc

--- src/Review.java
+++ src/Review.java
@@ -1,3 +1,8 @@
+/**
+* Author: Brett Logeais
+* Purpose: allows use to write reviews on an event
+*/
+
 import org.json.simple.JSONObject;
 
 public class Review extends DatabaseObject {
@@ -9,12 +14,21 @@ public class Review extends DatabaseObject {
 	private int rating;
 	private String comment;
 	
+	/**
+	* Purpose: creates new review from json
+	*/
 	public Review(JSONObject objectJSON) {
 		this.user = (String)objectJSON.get(USER);
 		this.rating = (int)(long)objectJSON.get(RATING);
 		this.comment = (String)objectJSON.get(COMMENT);
 	}
 	
+	/**
+	* Purpose: creates new review with user, rating, and comment
+	* @param user
+	* @param rating
+	* @param comment
+	*/
 	public Review(String user, int rating, String comment) {
 		this.user = user;
 		this.rating = rating;
@@ -28,6 +42,10 @@ public class Review extends DatabaseObject {
 		reviewDetails.put(COMMENT, this.comment);
 		return reviewDetails;
 	}
+	/**
+	* Purpose: to print a review
+	* @return String of the review
+	*/
 	public String toString() {
 		return "Review by " + this.user + "\n" + this.rating + " Stars\n" + this.comment;
 	}
