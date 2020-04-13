96cb597a9535e9c33b8a5f895e8e9d3ff590cc3f
elizabethstevenson <60235792+elizabethstevenson@users.noreply.github.com>
2020-04-13 12:26:30 -0400

    added javadoc

--- src/InventoryTest.java
+++ src/InventoryTest.java
@@ -1,3 +1,8 @@
+/**
+ * @author Elizabeth Stevenson
+ * Purpose: test methods in Inventory that are specific to itself
+ */
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
@@ -8,6 +13,9 @@ class InventoryTest {
 
 	public static Inventory testInventory;
 	
+	/**
+	 * Purpose: test getRewardsPoints
+	 */
 	@Test
 	public void testGetRewardsPoints() {
 		testInventory = new Inventory();
@@ -15,10 +23,23 @@ class InventoryTest {
 		assertEquals(testInventory.getRewardsPoints(), 10);
 	}
 	
+	/**
+	 * Purpose: test setRewardsPoints with positive value
+	 */
 	@Test
 	public void testSetRewardsPoints() {
 		testInventory = new Inventory();
 		testInventory.setRewardsPoints(10);
 		assertEquals(testInventory.getRewardsPoints(), 10);
+	}
+	
+	/**
+	 * Purpose: test setRewardsPoints with negative value
+	 */
+	@Test
+	public void testSetNegativeRewardsPoints() {
+		testInventory = new Inventory();
+		testInventory.setRewardsPoints(-10);
+		assertEquals(testInventory.getRewardsPoints(), -10);
 	}
 }
